8d0d6834cd6b4cf5a77911ab6ccbcb75a424b61a
Shench35 <akinpelushuaib0@gmail.com>
2026-04-22 15:03:03 +0100

    Kayode starts the backend today

---
+++ src/Backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+
+
+app = FastAPI(
+        title="CRYPTO TRADING BOT",
+    description="A bot to help you watch you pen trades and make prediction based on live market.",
+    version="1.0.0"
+)
+
